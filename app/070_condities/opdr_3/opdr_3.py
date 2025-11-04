267973248312f869fddc5167146197dbc221421d
Selin Gezer <97049968@st.deltion.nl>
2025-11-04 20:51:09 +0100

    -

--- app/070_condities/opdr_3/opdr_3.py
+++ app/070_condities/opdr_3/opdr_3.py
@@ -2,9 +2,6 @@
 # Naam student:
 # Groep:
 
-
-
-
 normale_toegangsprijs = 12.50
 kortings_percentages = {"baby": 100, "kinderen": 50, "volwassenen": 0, "ouderen": 30}
 leeftijd = {"baby": (0, 2), "kinderen": (3, 18), "volwassenen": (19, 64), "ouderen": (65, 150)}
@@ -27,10 +24,8 @@ else:
     groep = "onbekend"
     korting = 0
 
-# Bereken de te betalen prijs
 prijs = normale_toegangsprijs * (1 - korting / 100)
 
-# Print de resultaten
 print(f"U behoort tot de groep {groep}")
 print(f"U krijgt {korting}% korting")
 print(f"U betaalt {prijs}")
